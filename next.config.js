a4e5e8d8469959c2ee8b275482508dd699fe701a
Admin <admin@school.edu>
2026-06-07 00:41:23 +0700

    Add webpack alias for Render build

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['yahoo-finance2'],
   },
+  webpack(config) {
+    config.resolve.alias['@'] = __dirname
+    return config
+  },
 }
 
 module.exports = nextConfig
